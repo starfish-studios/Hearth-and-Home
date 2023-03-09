9ee42b07cd6b6fe9adeb3db880670c3dd1304659
Synthestra <blearsjace@gmail.com>
2023-03-09 02:43:09 +0000

    directional column blocks

--- common/src/main/java/com/crispytwig/hearth_and_home/block/ColumnBlock.java
+++ common/src/main/java/com/crispytwig/hearth_and_home/block/ColumnBlock.java
@@ -3,15 +3,21 @@ package com.crispytwig.hearth_and_home.block;
 import com.crispytwig.hearth_and_home.block.properties.ModBlockStateProperties;
 import com.crispytwig.hearth_and_home.block.properties.ColumnType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BrewingStandBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class ColumnBlock extends Block {
+    public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
     public static final EnumProperty<ColumnType> TYPE = ModBlockStateProperties.COLUMN_TYPE;
 
     public ColumnBlock(Properties properties) {
@@ -23,39 +29,47 @@ public class ColumnBlock extends Block {
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        BlockState blockstate = this.defaultBlockState().setValue(TYPE, ColumnType.NONE);
         Level level = context.getLevel();
-        BlockPos clickedPos = context.getClickedPos();
-        blockstate = blockstate.setValue(TYPE, getType(blockstate, level.getBlockState(clickedPos.above()), level.getBlockState(clickedPos.below())));
-        return blockstate;
+        BlockPos pos = context.getClickedPos();
+        Direction.Axis axis = context.getClickedFace().getAxis();
+
+        BlockState state = this.defaultBlockState().setValue(AXIS, axis);
+        state = state.setValue(TYPE, getType(state, getRelativeTop(level, pos, axis), getRelativeBottom(level, pos, axis)));
+        return state;
     }
 
     @Override
     public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
         if (level.isClientSide) return;
-        ColumnType type = getType(state, level.getBlockState(pos.above()), level.getBlockState(pos.below()));
-        if (state.getValue(TYPE) != type) {
-            state = state.setValue(TYPE, type);
-        }
+
+        Direction.Axis axis = state.getValue(AXIS);
+        ColumnType type = getType(state, getRelativeTop(level, pos, axis), getRelativeBottom(level, pos, axis));
+        if (state.getValue(TYPE) == type) return;
+
+        state = state.setValue(TYPE, type);
         level.setBlock(pos, state, 3);
     }
 
-    public ColumnType getType(BlockState state, BlockState above, BlockState below) {
-        boolean shape_above_same = above.is(state.getBlock());
-        boolean shape_below_same = below.is(state.getBlock());
+    public BlockState getRelativeTop(Level level, BlockPos pos, Direction.Axis axis) {
+        return level.getBlockState(pos.relative(Direction.fromAxisAndDirection(axis, Direction.AxisDirection.POSITIVE)));
+    }
 
-        if (shape_above_same && !shape_below_same) {
-            return ColumnType.BOTTOM;
-        } else if (!shape_above_same && shape_below_same) {
-            return ColumnType.TOP;
-        } else if (shape_above_same) {
-            return ColumnType.MIDDLE;
-        }
+    public BlockState getRelativeBottom(Level level, BlockPos pos, Direction.Axis axis) {
+        return level.getBlockState(pos.relative(Direction.fromAxisAndDirection(axis, Direction.AxisDirection.NEGATIVE)));
+    }
+
+    public ColumnType getType(BlockState state, BlockState above, BlockState below) {
+        boolean shape_above_same = above.is(state.getBlock()) && state.getValue(AXIS) == above.getValue(AXIS);
+        boolean shape_below_same = below.is(state.getBlock()) && state.getValue(AXIS) == below.getValue(AXIS);
+
+        if (shape_above_same && !shape_below_same) return ColumnType.BOTTOM;
+        else if (!shape_above_same && shape_below_same) return ColumnType.TOP;
+        else if (shape_above_same) return ColumnType.MIDDLE;
         return ColumnType.NONE;
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(TYPE);
+        builder.add(TYPE, AXIS);
     }
 }
